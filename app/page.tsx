ef46621606aece51979e281982fcefb88b1f0f6e
mathew <amonline2005@gmail.com>
2026-03-08 01:18:14 +0530

    feat:Navbar kinda okish

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
 import Login from '@/components/forms/Login'
 import Signup from '@/components/forms/SignUp'
+import Navbar from '@/components/shared/Navbar'
 
 export default function Home() {
   return (
     <>
-      <div className='bg-[#ccc]'>
-        <Signup />
+      <div className='bg-white'>
+        <Navbar />
+        <Login />
+        <Login />
+        <Login />
+        <Login />
       </div>
     </>
   )
